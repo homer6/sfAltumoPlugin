0046c38d98b86ef7061f3c4dfc6326e1e850d4a2
Juan Jaramillo <juan@cxdmail.com>
2011-07-14 19:50:45 -0700

    Fixed lib name inside of schema.xml; changed sfAltumo to sfAltumoPlugin

---
+++ lib/filter/SystemEventFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * SystemEvent filter form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ */
+class SystemEventFormFilter extends BaseSystemEventFormFilter
+{
+  public function configure()
+  {
+  }
+}
