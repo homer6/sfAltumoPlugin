c5caf30b80a4e82304eb39dc8a0896984d74290c
Juan Jaramillo <juan@cxdmail.com>
2011-07-16 20:01:37 -0700

    Working PropelSchemaCompiler

--- lib/Schema/PropelSchemaCompiler.php
+++ lib/Schema/PropelSchemaCompiler.php
@@ -25,13 +25,44 @@ namespace sfAltumoPlugin\Schema;
 * In order to get around that, the PropelSchemaCompiler class can automatically append
 * these foreign keys and output new schema files for propel to process.
 * 
+* Known Issues / Notes:
+* 
+*   -   Assumes primary keys are named "id"
+*   -   Results are unexpected if getCompiledDatabaseElement is called more than once
+* 
+* 
+* 
 * @author Juan Jaramillo <juan.jaramillo@altumo.com>
 */
 class PropelSchemaCompiler{
     
-    protected $merged_schema_xml = null;
-    
+    protected $table_elements = array();
+    protected $is_compiled = false;
 
+    
+    /**
+    * Setter for the $is_compiled attribute
+    * of this PropelSchemaCompiler
+    * 
+    * @param bool $is_compiled
+    */
+    protected function setCompiled( ){
+        $this->is_compiled = true;
+    }
+        
+        
+    /**
+    * Getter for the $is_compiled attribute
+    * of this PropelSchemaCompiler
+    * 
+    * @return bool
+    */
+    protected function isCompiled( ){
+        return (bool)$this->is_compiled;
+    }
+    
+    
+    
     /**
     * Initialize a new PropelSchemaCompiler
     * 
@@ -68,10 +99,236 @@ class PropelSchemaCompiler{
         
         $xml_document = new \sfAltumoPlugin\Schema\PropelSchemaFile( $xml_schema_path );
         
-        \Altumo\Utils\Debug::dump( $xml_document->getDatabaseElementData() );
+        $table_elements = $xml_document->getTableElements();
+        
+        $this->addTableElements( $table_elements );
         
     }
 
     
+    /**
+    * Add a <table> element (\Altumo\Xml\XmlElement) to the current set of 
+    * tables.
+    * 
+    * The array is indexed by table name.
+    * 
+    * @param Array $table_elements    // of \Altumo\Xml\XmlElement
+    *                                 //  table elements to add
+    * @return void
+    * 
+    * @throws \Exception              // If argument is not an array
+    *                                 // If a duplicate table name is found
+    *                                 // If one of the <table> nodes doesn't
+    *                                     have a "name" attribute.
+    */
+    protected function addTableElements( $table_elements ){
 
+        if( empty( $table_elements ) ){
+            return;
+        }
+        
+        if( !is_array( $table_elements ) ){
+            throw new \Exception( 'addTableElements expects an array' );
+        }
+        
+        foreach( $table_elements as $table_element ){
+            
+            $table_name = $table_element->xpath( '@name', true );
+            
+            $this->table_elements[ $table_name ] = $table_element;
+            
+        }
+        
+    }
+    
+    
+    /**
+    * Get array of current <table> elements (\Altumo\Xml\XmlElement).
+    * 
+    * @return Array         // of \Altumo\Xml\XmlElement
+    *                       //  indexes being table names
+    */
+    protected function &getTableElements(){
+    
+        return $this->table_elements;
+        
+    }
+    
+    
+    /**
+    * Parses the schema data and builds a map of all tables that inherit from
+    * other tables via concrete inheritance.
+    * 
+    * The resulting array is indexed by parent table names and the values are
+    * tables that are extending them.
+    * 
+    * @return Array
+    */
+    protected function getTableConcreteInheritanceMap(){
+        
+        $table_elements = &$this->getTableElements();
+        
+        // This array holds parent tables (tables being extended) as indexes
+        // and the tables extending them as values.
+            $table_inheritance_map = array();
+            
+
+        // Fill the table inheritance map.
+            foreach( $table_elements as $table_name => $table_element ){
+
+                if(0) $table_element = new \Altumo\Xml\XmlElement();
+
+                // Get all (parent) tables that are being extended by other tables.
+                    $concrete_inheritance_parent_tables = $table_element->queryWithXpath( 
+                        'behavior[@name="concrete_inheritance"]/parameter[@name="extends"]/@value', 
+                        \Altumo\Xml\XmlElement::RETURN_TYPE_STRING,
+                        false
+                    );
+
+                // Save in table inheritance map
+                    foreach( $concrete_inheritance_parent_tables as $parent_table ){
+                        $table_inheritance_map[$parent_table][] = $table_name;
+                    }
+            }
+        
+        return $table_inheritance_map;
+    }
+    
+    
+    
+    
+    /**
+    * Finds tables with missing concrete_inheritance foreign keys
+    * (on the table being extended) and adds them.
+    * 
+    * @return void
+    * 
+    * @throws \Exception            // if a column by the same name as the foreign key
+    *                               //  being created already exists.
+    *                               //  (only if the column is not already a foreign key)                              
+    *                               // if a referenced parent table is not
+    *                               //  defined in the schema.
+    */
+    protected function addConcreteInheritanceForeignKeys(){
+
+        $table_elements = &$this->getTableElements();
+        
+        $table_inheritance_map = $this->getTableConcreteInheritanceMap();
+
+        
+        foreach( $table_inheritance_map as $parent_table_name => $child_tables ){
+            
+            foreach( $child_tables as $child_table_name ){
+                
+                if( !isset( $table_elements[$parent_table_name] ) ){
+                    throw new \Exception( "Attempted to inherit from an undefined table \"{$parent_table_name}\"" );
+                }
+                
+                $parent_table_element = &$table_elements[$parent_table_name];
+                
+                if(0) $parent_table_element = new \Altumo\Xml\XmlElement();
+                
+                $foreign_key_name = $child_table_name . '_id';
+
+                
+                // Ensure no column with the same name as the FK exists on the parent table and is not a foreign key already.
+                    $existing_column = $parent_table_element->queryWithXpath( 
+                        'column[@name="' . $foreign_key_name . '"]',
+                        \Altumo\Xml\XmlElement::RETURN_TYPE_XML_ELEMENT,
+                        false
+                    );
+                    
+                    if( !empty( $existing_column ) ){
+
+                        // A column exists. Ensure it's a foreign key to the parent table.
+                            $existing_fk = $parent_table_element->queryWithXpath( 
+                                'foreign-key[@foreignTable="' . $child_table_name . '"]/reference[@local="' . $foreign_key_name . '"]',
+                                \Altumo\Xml\XmlElement::RETURN_TYPE_XML_ELEMENT,
+                                false
+                            );
+                        
+                        if( empty( $existing_fk ) ){
+                            
+                            throw new \Exception( "A column \"{$foreign_key_name}\" already exists in table \"{$parent_table_name}\". 
+                                                    However this column is not a foreign key to {$child_table_name}. 
+                                                    Either remove/rename this colum or make it a foreign key for Concrete Inheritance." 
+                            );
+                            
+                        }
+                        
+                    }
+                    
+                
+                // Add foreign key
+                
+                    $column_element = $parent_table_element->addChild( 'column' );
+                        $column_element->addAttribute( 'name', $foreign_key_name );
+                        $column_element->addAttribute( 'required', 'false' );
+                        $column_element->addAttribute( 'type', 'integer' );
+
+                    $foreign_key_element = $parent_table_element->addChild( 'foreign-key' );
+                        $foreign_key_element->addAttribute( 'foreignTable', $child_table_name );
+                        $foreign_key_element->addAttribute( 'onDelete', 'cascade' );
+                        
+                        $foreign_key_reference_element = $foreign_key_element->addChild( 'reference' );
+                            $foreign_key_reference_element->addAttribute( 'local', $foreign_key_name );
+                            $foreign_key_reference_element->addAttribute( 'foreign', 'id' );
+                
+            }
+        }
+    }
+    
+    
+    
+    /**
+    * Executes all of the compilation procedures
+    * 
+    * Does not execute if this schema has already been compiled.
+    * 
+    * @return void
+    */
+    protected function compileSchema(){
+        
+        if( $this->isCompiled() ){
+            return;
+        }
+        
+        $this->addConcreteInheritanceForeignKeys();
+        
+        $this->setCompiled();
+        
+    }
+    
+    
+    /**
+    * Returns a <database> element containing all the tables after processing them.
+    * 
+    * @return \Altumo\Xml\XmlElement
+    */
+    public function getCompiledDatabaseElement(){
+    
+        $this->compileSchema();
+        
+        $table_elements = &$this->getTableElements();
+        
+        
+        // TODO: Refactor \Altumo\Xml\XmlElement so that it allows for creating
+        // a new element and adding children from existing \Altumo\Xml\XmlElement
+        
+        $xml_schema_output = '<database name="propel" defaultIdMethod="native" noXsd="true" package="lib.model">';
+        
+        foreach( $table_elements as $table_element ){
+           
+            $xml_schema_output .= "\n" . $table_element->getXmlAsString();
+            
+        }
+        
+        $xml_schema_output .= '</database>';
+        
+        $database_element = new \Altumo\Xml\XmlElement( $xml_schema_output );
+        
+        return $database_element;
+        
+    }
+    
 }
--- lib/Schema/PropelSchemaFile.php
+++ lib/Schema/PropelSchemaFile.php
@@ -17,6 +17,11 @@ namespace sfAltumoPlugin\Schema;
 * used for performing certain modifications to the schema before Propel
 * can use it to build the schema.
 * 
+* Known Issues:
+* 
+*   -   Does not support Propel's <external-schema>
+*       http://www.propelorm.org/wiki/Documentation/1.6/Schema#external-schemaelement
+* 
 * @see PropelSchemaCompiler.php
 * 
 * @author Juan Jaramillo <juan.jaramillo@altumo.com>
@@ -45,275 +50,21 @@ class PropelSchemaFile extends \Altumo\Xml\XmlFile {
     
     
     /**
+    * Returns all <table> elemenets for this schema file.
+    * The elements are returned as an array of \Altumo\Xml\XmlElement
     * 
-    * @return \Altumo\Xml\XmlElement
-    * 
+    * @return Array     // of \Altumo\Xml\XmlElement
     */
-    public function getDatabaseElement(){
+    public function getTableElements(){
         
         $this->assertFileOpen();
         
         $xml_root = $this->getXmlRoot();
         
-        $database_element = $xml_root->queryWithXpath( '/database', \Altumo\Xml\XmlElement::RETURN_TYPE_XML_ELEMENT );
+        $database_element = $xml_root->queryWithXpath( '/database/table', \Altumo\Xml\XmlElement::RETURN_TYPE_XML_ELEMENT );
        
-        return  reset( $database_element );
-        
-    }
-    
-    
-    public function getDatabaseElementData(){
-        
-        return $this->getDatabaseElement()->getXmlAsString( false );
-        
-    }
-    
-    
-    
-    
-    
-    /**
-    * Gets the default empty file as an xml string.
-    * 
-    * @return string
-    */
-    protected function getDefaultEmptyFile(){
-        
-
-        
-        
-        return '<BuildSequence/>';
-        
-    }
-    
-    
-    /**
-    * Adds a build sequence change.
-    * 
-    * 
-    * @param string $hash
-    * @param boolean $upgrade
-    * @param boolean $drop
-    * @param boolean $snapshot
-    * 
-    */    
-    public function addChange( $hash, $upgrade = null, $drop = null, $snapshot = null ){
-        
-        $this->assertFileOpen();
-        $this->assertFileWritable();
-        
-        $xml_root = $this->getXmlRoot();
-            $change = $xml_root->addChild('Change');
-            
-                $change->addAttribute( 'hash', $hash );
-                            
-                if( !is_null($upgrade) ){
-                    if( $upgrade ){
-                        $change->addAttribute( 'upgrade', 'true' );
-                    }else{
-                        $change->addAttribute( 'upgrade', 'false' );
-                    }
-                }
-            
-                if( !is_null($drop) ){
-                    if( $drop ){
-                        $change->addAttribute( 'drop', 'true' );
-                    }else{
-                        $change->addAttribute( 'drop', 'false' );
-                    }
-                }
-            
-                if( !is_null($snapshot) ){
-                    if( $snapshot ){
-                        $change->addAttribute( 'snapshot', 'true' );
-                    }else{
-                        $change->addAttribute( 'snapshot', 'false' );
-                    }
-                }
-        
-        
-    }
-    
-    
-    /**
-    * Gets the hash of the latest snapshot
-    * 
-    * @return string
-    */
-    public function getLastestSnapshotHash(){
-        
-        $this->assertFileOpen();
-        return $this->getXmlRoot()->xpath( 'Change[@snapshot="true"][last()]/attribute::hash', false );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that an upgrade 
-    * script was present in) since (but not including)
-    * the supplied commit hash.
-    * 
-    * @param $since_hash
-    * @return array
-    */
-    public function getUpgradeHashesSince( $since_hash ){
-        
-        return $this->getHashesSince( $since_hash, 'upgrade' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that an snapshot 
-    * script was present in) since (but not including)
-    * the supplied commit hash.
-    * 
-    * @param $since_hash
-    * @return array
-    */
-    public function getSnapshotHashesSince( $since_hash ){
-        
-        return $this->getHashesSince( $since_hash, 'snapshot' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that a drop 
-    * script was present in) since (but not including)
-    * the supplied commit hash.
-    * 
-    * @param $since_hash
-    * @return array
-    */
-    public function getDropHashesSince( $since_hash ){
-        
-        return $this->getHashesSince( $since_hash, 'drop' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that an upgrade 
-    * script was present in) before (AND including the current hash)
-    * the supplied commit hash.
-    * 
-    * @param $before_hash
-    * @return array
-    */
-    public function getUpgradeHashesBefore( $before_hash ){
-        
-        return $this->getHashesBefore( $before_hash, 'upgrade' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that a snapshot 
-    * script was present in) before (AND including the current hash)
-    * the supplied commit hash.
-    * 
-    * @param $before_hash
-    * @return array
-    */
-    public function getSnapshotHashesBefore( $before_hash ){
-        
-        return $this->getHashesBefore( $before_hash, 'snapshot' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that a drop 
-    * script was present in) before (AND including the current hash)
-    * the supplied commit hash.
-    * 
-    * @param $before_hash
-    * @return array
-    */
-    public function getDropHashesBefore( $before_hash ){
-        
-        return $this->getHashesBefore( $before_hash, 'drop' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that an $attribute 
-    * script was present in) since (but not including)
-    * the supplied commit hash.
-    * 
-    * @param string $since_hash
-    * @param string $attribute
-    * @return array
-    */
-    protected function getHashesSince( $since_hash, $attribute ){
-        
-        $this->assertFileOpen();
-        
-        if( !in_array($attribute, $this->getValidAttributes()) ){
-            throw new \Exception('Invalid attribute.');
-        }
-        
-        //if the "since" hash was not found, set the hash position to so that it will match all entries
-        if( count( $this->getXmlRoot()->queryWithXpath( 'Change[@hash="' . $since_hash . '"]', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false ) ) == 0 ){
-            $hash_position = 0;
-        }else{
-            //get the position of the since_hash
-            $hash_position = count( $this->getXmlRoot()->queryWithXpath( 'Change[@hash="' . $since_hash . '"]/preceding-sibling::*', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false ) ) + 1;
-        }
-        
-        //get the changes
-        $changes = $this->getXmlRoot()->queryWithXpath( 'Change[position() > ' . $hash_position . '][@' . $attribute . '="true"]/attribute::hash', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false );        
-        
-        return $changes;
-        
-    }
-    
-    
-    /**
-    * Gets an array of the valid attibutes a Change element may have
-    * 
-    * @return array
-    */
-    protected function getValidAttributes(){
-        
-        return array( 'upgrade', 'drop', 'snapshot' );
-        
-    }
-    
-    
-    /**
-    * Gets an array of commit hashes (that an $attribute 
-    * script was present in) before (AND including)
-    * the supplied commit hash.
-    * 
-    * @param string $before_hash
-    * @param string $attribute
-    * @return array
-    */
-    protected function getHashesBefore( $before_hash, $attribute ){
-        
-        $this->assertFileOpen();
-        
-        if( !in_array($attribute, $this->getValidAttributes()) ){
-            throw new \Exception('Invalid attribute.');
-        }
-        
-        //if the "since" hash was not found, set the hash position to so that it will match all entries
-        if( count( $this->getXmlRoot()->queryWithXpath( 'Change[@hash="' . $before_hash . '"]', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false ) ) == 0 ){
-            $hash_position = count( $this->getXmlRoot()->queryWithXpath( 'Change', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false ) );
-        }else{
-            //get the position of the before_hash
-            $hash_position = count( $this->getXmlRoot()->queryWithXpath( 'Change[@hash="' . $before_hash . '"]/preceding-sibling::*', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false ) ) + 1;
-        }
-        
-        //get the changes
-        $changes = $this->getXmlRoot()->queryWithXpath( 'Change[position() <= ' . $hash_position . '][@' . $attribute . '="true"]/attribute::hash', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING, false );        
-        
-        return $changes;
+        return  $database_element;
         
     }
 
-    
 }
