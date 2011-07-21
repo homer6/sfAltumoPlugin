a5b4a5e51ee5397989d43db9d5a3d54ee92ba202
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-07-21 13:50:47 -0700

    Fixed git hook

--- install/git_hook_handler.sh
+++ install/git_hook_handler.sh
@@ -7,4 +7,7 @@ SCRIPT=$(readlink -f $0)
 PROJECT_PATH=`dirname $SCRIPT`/../../htdocs/project
 HOOK_NAME=$(basename $SCRIPT)
 
+# unset the $GIT_DIR because git hooks are setting it to ".git"
+# see: http://debuggable.com/posts/git-tip-auto-update-working-tree-via-post-receive-hook:49551efe-6414-4e86-aec6-544f4834cda3
+unset GIT_DIR
 $PROJECT_PATH/symfony altumo:git-hook-handler $HOOK_NAME
