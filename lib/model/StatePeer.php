964ea6df3a4cb028fa725b9f62929c022e4fb8ba
Juan Jaramillo <juan@cxdmail.com>
2011-07-25 13:10:00 -0700

    Updated altumo

--- lib/model/StatePeer.php
+++ lib/model/StatePeer.php
@@ -14,5 +14,38 @@
  * @package    propel.generator.plugins.sfAltumoPlugin.lib.model
  */
 class StatePeer extends BaseStatePeer {
-
+    
+    /**
+    * Find a State by name, and optionally filter by country.
+    * 
+    * @param mixed $name
+    * @param Country $country
+    * @return State
+    */
+    public static function retrieveByName( $name, $country = null ){
+        return StateQuery::create()
+            ->filterByName( $name )
+            ->_if( !is_null( $country ) )
+                ->filterByCountry( !is_null( $country ) ? $country : null )
+            ->_endif()
+        ->findOne();
+    }
+    
+    /**
+    * Find a State by iso (or short) code, and optionally filter by country.
+    * 
+    * @param mixed $name
+    * @param Country $country
+    * @return State
+    */
+    public static function retrieveByCode( $iso_code, $country = null ){
+        return StateQuery::create()
+            ->where( 'State.IsoCode LIKE ?', $iso_code )
+            ->orWhere( 'State.IsoShortCode LIKE ?', $iso_code )
+            ->_if( !is_null( $country ) )
+                ->filterByCountry( !is_null( $country ) ? $country : null )
+            ->_endif()
+        ->findOne();
+    }
+    
 } // StatePeer
