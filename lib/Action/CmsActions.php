2bfefe1c45a8a7203a1372dc31fb795ed8f7f4e7
Juan Jaramillo <juan@cxdmail.com>
2012-05-04 21:52:34 -0700

    CMS Wip

--- lib/Action/CmsActions.php
+++ lib/Action/CmsActions.php
@@ -20,6 +20,9 @@ namespace sfAltumoPlugin\Action;
 * @author Juan Jaramillo <juan.jaramillo@altumo.com>
 */
 class CmsActions extends BaseActions {
+    
+    protected $cms_buffer_count = 0;
+    protected $current_cms_fragment = null;
 
     
     public function preExecute(){
@@ -27,7 +30,7 @@ class CmsActions extends BaseActions {
         $this->addCmsFunctions();
         
     }
-    
+
     
     protected function userCanEditCmsFragments(){
         
@@ -40,23 +43,112 @@ class CmsActions extends BaseActions {
 
         $user_can_edit_cms_fragments = $this->userCanEditCmsFragments();
         
-        $this->cms_fragment = function( $tag, $chrome_attributes = '' ){
+        $that = $this;
+
+        $this->cms_start_fragment = function( $tag, $chrome_attributes = '' ) use ( $that ){
             
-            $chrome_attributes = \Altumo\Validation\Strings::assertString(
-                $chrome_attributes,
-                '$chrome_attributes expects a string'
-            );
+            return $that->startCmsFragment( $tag, $chrome_attributes );
             
+        };        
+        
+        $this->cms_end_fragment = function() use ( $that ){
+            
+            return $that->endCmsFragment();
+            
+        };
+
+    }
+
+
+    public function startCmsFragment( $tag, $chrome_attributes = '' ){
+
+        $chrome_attributes = \Altumo\Validation\Strings::assertString(
+            $chrome_attributes,
+            '$chrome_attributes expects a string'
+        );
+
+        // prevent nested fragments
+            if( $this->hasCurrentCmsFragment() ){
+                throw new \Exception( 'Already inside "' . $this->getCurrentCmsFragment()->getTag() . '". Cannot start another fragment.' );
+            }
+
+        // retrieve fragment                                             
             $fragment = \CmsFragmentPeer::retrieveByTag( $tag );
-            
+
+        // if this is a new segment, create a new one.
             if( is_null($fragment) ){
                 $fragment = \CmsFragmentPeer::getStubCmsFragment( $tag );
             }
 
-            return $fragment->getHtml( $chrome_attributes );
+        // set chrome attributes
+            $fragment->setChromeAttributes( $chrome_attributes );
+            
+        // set current fragment
+            $this->setCurrentCmsFragment( $fragment );
 
-        };
+        // start output buffering
+            ob_start();
+            $this->cms_buffer_count++;
 
+    }
+    
+
+    public function endCmsFragment(){
+        
+        if( !$this->hasCurrentCmsFragment() ){
+            throw new Exception( 'Not currently inside of a fragment.' );
+        }
+        
+        // end output buffering
+            $buffer_contents = ob_get_contents();
+            ob_end_clean();
+            $this->cms_buffer_count--;
+            
+        // if this is a new fragment, save buffered content in it
+            $current_fragment = $this->getCurrentCmsFragment();
+            
+            if( $current_fragment->isNew() ){   
+                $current_fragment->setContent( $buffer_contents );
+                $current_fragment->save();
+            }
+
+        // output the content of the fragment
+            echo $current_fragment->getHtml();
+            
+        // end current fragment
+            $this->setCurrentCmsFragment( null );
+
+    }
+    
+    
+    protected function hasCurrentCmsFragment(){
+        
+        return null !== $this->getCurrentCmsFragment();
+    
+    }
+    
+    
+    protected function setCurrentCmsFragment( $cms_fragment ){
+        
+        $cms_fragment = \Altumo\Validation\Objects::assertObjectInstanceOfClassOrNull(
+            $cms_fragment,
+            'CmsFragment',
+            '$cms_fragment expects CmsFragment or null'
+        );
+        
+        $this->current_fragment = $cms_fragment;
+        
+    }    
+    
+    
+    /**
+    * Gets the CmsFragment that is currently being printed.
+    * 
+    * @return CmsFragment
+    */
+    protected function getCurrentCmsFragment(){
+        
+        return $this->current_fragment;
         
     }
 
--- modules/sfAltumoPlugin_cms_fragment/templates/_text.php
+++ modules/sfAltumoPlugin_cms_fragment/templates/_text.php
@@ -1,3 +1,6 @@
+<?php 
+    $chrome_element_attributes = html_entity_decode($chrome_element_attributes);
+?>  
 <div id="<?php echo $element_id; ?>" <?php echo $chrome_element_attributes; ?>>
     <?php echo htmlspecialchars_decode($content); ?>
 </div>
